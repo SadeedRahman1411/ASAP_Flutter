7b8d0f0efa63fd8686e71d8c042422a6a162def8
SadeedRahman1411 <133593088+SadeedRahman1411@users.noreply.github.com>
2024-07-07 12:34:52 -0700

    chat user interface correction done

--- lib/models/chat_user.dart
+++ lib/models/chat_user.dart
@@ -1,42 +1,48 @@
 class ChatUser {
   ChatUser({
-    required this.active,
-    required this.email,
+    required this.image,
     required this.about,
     required this.name,
-    required this.status,
+    required this.createdAt,
+    required this.isOnline,
+    required this.id,
+    required this.lastActive,
+    required this.email,
+    required this.pushToken,
   });
+  late String image;
+  late String about;
+  late String name;
+  late String createdAt;
+  late bool isOnline;
+  late String id;
+  late String lastActive;
+  late String email;
+  late String pushToken;
 
-  late final bool active;
-  late final String email;
-  late final String about;
-  late final String name;
-  late final String status;
-
-  ChatUser.fromJson(Map<String, dynamic>? json) {
-    if (json != null) {
-      active = json['Active'] ?? false; // Use false as default value for bool
-      email = json['Email'] ?? '';
-      about = json['about'] ?? '';
-      name = json['Name'] ?? '';
-      status = json['Status'] ?? '';
-    } else {
-      // Handle case where json is null (if required)
-      active = false;
-      email = '';
-      about = '';
-      name = '';
-      status = '';
-    }
+  ChatUser.fromJson(Map<String, dynamic> json) {
+    image = json['image'] ?? '';
+    about = json['about'] ?? '';
+    name = json['name'] ?? '';
+    createdAt = json['created_at'] ?? '';
+    isOnline = json['is_online'] ?? '';
+    id = json['id'] ?? '';
+    lastActive = json['last_active'] ?? '';
+    email = json['email'] ?? '';
+    pushToken = json['push_token'] ?? '';
   }
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
-    data['Active'] = active;
-    data['Email'] = email;
+    data['image'] = image;
     data['about'] = about;
-    data['Name'] = name;
-    data['Status'] = status;
+    data['name'] = name;
+    data['created_at'] = createdAt;
+    data['is_online'] = isOnline;
+    data['id'] = id;
+    data['last_active'] = lastActive;
+    data['email'] = email;
+    data['push_token'] = pushToken;
     return data;
   }
 }
